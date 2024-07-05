1e50daef3d6af6d868be016ba494d2946384b2a6
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2024-07-05 15:59:30 +0600

    first

---
+++ Nice_Kyrgyzstan/src/main/java/com/example/Nice_Kyrgyzstan/NiceKyrgyzstanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nice_Kyrgyzstan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceKyrgyzstanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceKyrgyzstanApplication.class, args);
+	}
+
+}
