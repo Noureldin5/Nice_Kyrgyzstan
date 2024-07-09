838afc046346d80822ee0ce3eff56f01d6cd3be9
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2024-07-09 16:46:08 +0600

    ignore

--- Nice_Kyrgyzstan/src/main/java/com/example/Nice_Kyrgyzstan/controller/EmailController.java
+++ Nice_Kyrgyzstan/src/main/java/com/example/Nice_Kyrgyzstan/controller/EmailController.java
@@ -2,10 +2,11 @@ package com.example.Nice_Kyrgyzstan.controller;
 
 import com.example.Nice_Kyrgyzstan.dto.EmailForm;
 import com.example.Nice_Kyrgyzstan.service.EmailService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import jakarta.validation.Valid;
+
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
